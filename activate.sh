7d5c481abe40b8957ca740b6ec7b2cd80935adfb
Michael Verhulst <michael@terminallabs.com>
2020-12-06 18:04:38 -0600

    Delete activate.sh

--- activate.sh
+++
@@ -1,7 +0,0 @@
-export APPNAME=hologram
-export USERNAME=user
-
-export PATH="/home/$USERNAME/miniconda3/bin:$PATH"
-source /home/$USERNAME/miniconda3/etc/profile.d/conda.sh;
-
-conda activate ${APPNAME}
